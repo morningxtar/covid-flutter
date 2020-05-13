297fddd902202c702040c1b6914a250cedd593ee
FranckKouamelan <lekouamelan@gmail.com>
2020-05-13 14:15:01 +0000

    first

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:covid19stat/screens/covid.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(Covid());
+
---
+++ lib/screens/covid.dart
@@ -0,0 +1,19 @@
+import 'package:covid19stat/screens/statistiques/first-statistique.dart';
+import 'package:flutter/material.dart';
+
+ThemeData appTheme = ThemeData(
+  fontFamily: 'Oxygen'
+);
+
+class Covid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Covid stats',
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: new Statistiques(),
+    );
+  }
+
+}
